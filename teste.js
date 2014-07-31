223c6bb63a386b4a04809331d9240e01b375cc89
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 13:17:56 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,8 +1,8 @@
 container = document.getElementsByClassName("add_site_item_sub");
-window.alert(container.length);
+
 for(var i = 0; i < 31; i++){
   var cb = document.createElement('input');
   cb.type = "checkbox";
   cb.id = i;
-  container[i+4].appendChild(cb);
+  container[i].appendChild(cb);
 }
