28f76bf308c90552acecfb91e38506047582b137
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 16:08:53 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,9 +1,6 @@
 console.log("v1");
 
 container = document.getElementsByClassName("add_site_item_sub");
-//var div = document.createElement('div');
-//div.align = "rigth";
-//container.appendChild(div);
 
 for(var i = 0; i < 1; i++){
   var cb = document.createElement('input');
