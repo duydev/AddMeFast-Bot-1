5e1856c0685d03dce6bfd7642eda3969b1410045
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 16:15:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ if(getCookie("amfstep") != "0,0")
   setCookie("amfstep","0");
 else
   step = getCookie("amfstep").split(',')[0];
-
+var onoff = [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 if(getCookie("amfonoff") != "0,0")
   setCookie("amfonoff","1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1");
 else
   step = getCookie("amfonoff").split(',')[0];
+
+function checks(){
+  container = document.getElementsByClassName("add_site_item_sub");
+  for(var i = 0; i < 1; i++){
+    var cb = document.createElement('input');
+    cb.type = "checkbox";
+    if(onoff[i] == 1)
+      cb.checked = true;
+    cb.id = i;
+    container[i].appendChild(cb);
+  }
+}
 
 function setCookie(nome,str){
   var expires = new Date();
