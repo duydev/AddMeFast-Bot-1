7aaa8c662cf79a552de34986203a75d693e726dc
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 13:06:55 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-container = document.getElementsByClassName("add_sites").getElementsByTagName("a");
+container = document.getElementsByClassName("add_sites")[0].getElementsByTagName("a");
 for(var i = 0; i < 31; i++){
   var cb = document.createElement('input');
   cb.type = "checkbox";
