f23a673ba8fcb8c476b64e6159e3c9cd92d5f496
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 16:09:55 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,10 +1,11 @@
-console.log("v1");
+console.log("v2");
 
-container = document.getElementsByClassName("add_site_item_sub");
-
-for(var i = 0; i < 1; i++){
-  var cb = document.createElement('input');
-  cb.type = "checkbox";
-  cb.id = i;
-  container[i].appendChild(cb);
+function checks(){
+  container = document.getElementsByClassName("add_site_item_sub");
+  for(var i = 0; i < 1; i++){
+    var cb = document.createElement('input');
+    cb.type = "checkbox";
+    cb.id = i;
+    container[i].appendChild(cb);
+  }
 }
