c431b469baaf88a83c638eadb17a5c303e011999
Xuxinha <filipe.bitt10@gmail.com>
2014-07-31 13:35:45 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -5,7 +5,7 @@ var div = document.createElement('div');
 div.align = "rigth";
 container.appendChild(div);
 
-for(var i = 0; i < 31; i++){
+for(var i = 0; i < 1; i++){
   var cb = document.createElement('input');
   cb.type = "checkbox";
   cb.id = i;
