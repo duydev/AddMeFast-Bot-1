64b9d1e67039e5b1e9ab70a08cfe8400c0f0010f
Xuxinha <filipe.bitt10@gmail.com>
2014-08-01 14:49:11 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,6 +1,20 @@
-document.getElementsByClassName("user-actions-follow-button js-follow-btn follow-button btn")[0].click();
-setTimeout(close,2000);
+var step = getCookie("amfstep").split(',')[0];
+console.log(step+document.getElementsByClassName("add_site_item_sub")[step].textContent);
+setCookie("amfstep",(step+1)+",0");
+document.getElementsByClassName("add_site_item_sub")[step].getElementsByTagName("a")[0].click();
 
-function close(){
-	windows.close();
+function setCookie(nome,str){
+  var expires = new Date();
+  expires.setTime(expires.getTime()+(30*24*60*60*1000));
+  expires = expires.toGMTString();
+  document.cookie = nome+"="+str+"; expires="+expires+"; path=/";
+}
+
+function getCookie(name){
+  var cookies = document.cookie.split("; ");
+  for(var i = 0; i < cookies.length; i++){
+    if(cookies[i].split("=")[0] == name)
+      return cookies[i].split("=")[1];
+  }
+  return "0,0";
 }
