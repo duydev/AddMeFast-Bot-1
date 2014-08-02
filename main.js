dcd89a5ab87e6ad0b40640feca0d3aaa23487b74
Xuxinha <filipe.bitt10@gmail.com>
2014-08-02 02:11:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,139 +1,98 @@
 console.log("AddMeFast v1");
 var url = window.location.href;
+var opt = 6;//Math.floor((Math.random() * 30) + 0);
+switch(opt){
+  case 0://Facebook Likes
 
-if(url.indexOf("addmefast") != null){
-  window.alert("Entrou 1");
-  var step = parseInt(getCookie("amfstep").split(',')[0]);
-  var opt = parseInt(getCookie("amfstep").split(',')[1]);
-  switch(step){
-    case 0:
-      opt = 6;//Math.floor((Math.random() * 30) + 0);
-      setCookie("amfstep","1,"+opt);
-      document.getElementsByClassName("add_site_item_sub")[opt].getElementsByTagName("a")[0].click();
-    break;
-    case 1:
-      setCookie("amfstep","2,"+opt);
-      document.getElementsByClassName("btn3")[1].click();
-    break;
-    case 2:
-      setCookie("amfstep","0,0");
-    break;
-  }
-}
-else{
-  window.alert("Entrou 2");
-  switch(opt){
-    case 0://Facebook Likes
+  break;
+  case 1://Facebook Followers
 
-    break;
-    case 1://Facebook Followers
+  break;
+  case 2://Facebook Post Like
 
-    break;
-    case 2://Facebook Post Like
+  break;
+  case 3://Facebook Post Share
 
-    break;
-    case 3://Facebook Post Share
+  break;
+  case 4://Instagram Followers
 
-    break;
-    case 4://Instagram Followers
+  break;
+  case 5://Instagram Likes
 
-    break;
-    case 5://Instagram Likes
+  break;
+  case 6://Twitter Followers
+    if(url != "http://addmefast.com/free_points/twitter")
+      window.open("http://addmefast.com/free_points/twitter", "_self");
+    document.getElementsByClassName("user-actions-follow-button js-follow-btn follow-button btn")[0].click();
+    setTimeout(close,2000);
+  break;
+  case 7://Twitter Tweets
 
-    break;
-    case 6://Twitter Followers
-      document.getElementsByClassName("user-actions-follow-button js-follow-btn follow-button btn")[0].click();
-      setTimeout(close,2000);
-    break;
-    case 7://Twitter Tweets
+  break;
+  case 8://Twitter Retweets
 
-    break;
-    case 8://Twitter Retweets
+  break;
+  case 9://Twitter Favorites
 
-    break;
-    case 9://Twitter Favorites
+  break;
+  case 10://YouTube Subscribe
 
-    break;
-    case 10://YouTube Subscribe
+  break;
+  case 11://YouTube Likes
 
-    break;
-    case 11://YouTube Likes
+  break;
+  case 12://YouTube Favorites
 
-    break;
-    case 12://YouTube Favorites
+  break;
+  case 13://YouTube Views
 
-    break;
-    case 13://YouTube Views
+  break;
+  case 14://Google Circles
 
-    break;
-    case 14://Google Circles
+  break;
+  case 15://Google Share
 
-    break;
-    case 15://Google Share
+  break;
+  case 16://Ask.fm Likes
 
-    break;
-    case 16://Ask.fm Likes
+  break;
+  case 17://Vine Followers
 
-    break;
-    case 17://Vine Followers
+  break;
+  case 18://Vine Likes
 
-    break;
-    case 18://Vine Likes
+  break;
+  case 19://Vine Revines
 
-    break;
-    case 19://Vine Revines
+  break;
+  case 20://Vkontakte Pages
 
-    break;
-    case 20://Vkontakte Pages
+  break;
+  case 21://Vkontakte Groups
 
-    break;
-    case 21://Vkontakte Groups
+  break;
+  case 22://MySpace Friends
 
-    break;
-    case 22://MySpace Friends
+  break;
+  case 23://Pinterest Followers
 
-    break;
-    case 23://Pinterest Followers
+  break;
+  case 24://Pinterest Repins
 
-    break;
-    case 24://Pinterest Repins
+  break;
+  case 25://Pinterest Likes
 
-    break;
-    case 25://Pinterest Likes
+  break;
+  case 26://Reverbnation Fans
 
-    break;
-    case 26://Reverbnation Fans
+  break;
+  case 27://SoundCloud Follow
 
-    break;
-    case 27://SoundCloud Follow
+  break;
+  case 28://SoundCloud Plays
 
-    break;
-    case 28://SoundCloud Plays
+  break;
+  case 29://StumbleUpon
 
-    break;
-    case 29://StumbleUpon
-
-    break;
-  }
-  setCookie("amfstep","0,0");
-}
-
-function close(){
-  windows.close();
-}
-
-function setCookie(nome,str){
-  var expires = new Date();
-  expires.setTime(expires.getTime()+(30*24*60*60*1000));
-  expires = expires.toGMTString();
-  document.cookie = nome+"="+str+"; expires="+expires+"; path=/";
-}
-
-function getCookie(name){
-  var cookies = document.cookie.split("; ");
-  for(var i = 0; i < cookies.length; i++){
-    if(cookies[i].split("=")[0] == name)
-      return cookies[i].split("=")[1];
-  }
-  return "0,0";
+  break;
 }
