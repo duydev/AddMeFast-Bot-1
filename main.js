e26575a781e4f68a40c16997016616037d3bdb87
Xuxinha <filipe.bitt10@gmail.com>
2014-08-02 02:25:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,98 +1,110 @@
-console.log("AddMeFast v1");
+console.log("AddMeFast v3");
 var url = window.location.href;
-var opt = 6;//Math.floor((Math.random() * 30) + 0);
+var opt = 7;//Math.floor((Math.random() * 30) + 0);
+
 switch(opt){
   case 0://Facebook Likes
 
   break;
-  case 1://Facebook Followers
+  case 1://Facebook Share
 
   break;
-  case 2://Facebook Post Like
+  case 2://Facebook Followers
 
   break;
-  case 3://Facebook Post Share
+  case 3://Facebook Post Like
 
   break;
-  case 4://Instagram Followers
+  case 4://Facebook Post Share
 
   break;
-  case 5://Instagram Likes
+  case 5://Instagram Followers
 
   break;
-  case 6://Twitter Followers
+  case 6://Instagram Likes
+
+  break;
+  case 7://Twitter Followers
     if(url != "http://addmefast.com/free_points/twitter")
       window.open("http://addmefast.com/free_points/twitter", "_self");
-    document.getElementsByClassName("user-actions-follow-button js-follow-btn follow-button btn")[0].click();
-    setTimeout(close,2000);
+    if(url.indexOf("addmefast") != null)
+      document.getElementsByClassName("btn3")[1].click();
+    else{
+      document.getElementsByClassName("user-actions-follow-button js-follow-btn follow-button btn")[0].click();
+      setTimeout(close,1500);
+    }
   break;
-  case 7://Twitter Tweets
+  case 8://Twitter Tweets
 
   break;
-  case 8://Twitter Retweets
+  case 9://Twitter Retweets
 
   break;
-  case 9://Twitter Favorites
+  case 10://Twitter Favorites
 
   break;
-  case 10://YouTube Subscribe
+  case 11://YouTube Subscribe
 
   break;
-  case 11://YouTube Likes
+  case 12://YouTube Likes
 
   break;
-  case 12://YouTube Favorites
+  case 13://YouTube Favorites
 
   break;
-  case 13://YouTube Views
+  case 14://YouTube Views
 
   break;
-  case 14://Google Circles
+  case 15://Google Circles
 
   break;
-  case 15://Google Share
+  case 16://Google Share
 
   break;
-  case 16://Ask.fm Likes
+  case 17://Ask.fm Likes
 
   break;
-  case 17://Vine Followers
+  case 18://Vine Followers
 
   break;
-  case 18://Vine Likes
+  case 19://Vine Likes
 
   break;
-  case 19://Vine Revines
+  case 20://Vine Revines
 
   break;
-  case 20://Vkontakte Pages
+  case 21://Vkontakte Pages
 
   break;
-  case 21://Vkontakte Groups
+  case 22://Vkontakte Groups
 
   break;
-  case 22://MySpace Friends
+  case 23://MySpace Friends
 
   break;
-  case 23://Pinterest Followers
+  case 24://Pinterest Followers
 
   break;
-  case 24://Pinterest Repins
+  case 25://Pinterest Repins
 
   break;
-  case 25://Pinterest Likes
+  case 26://Pinterest Likes
 
   break;
-  case 26://Reverbnation Fans
+  case 27://Reverbnation Fans
 
   break;
-  case 27://SoundCloud Follow
+  case 28://SoundCloud Follow
 
   break;
-  case 28://SoundCloud Plays
+  case 29://SoundCloud Plays
 
   break;
-  case 29://StumbleUpon
+  case 30://StumbleUpon
 
   break;
+}
+
+function close(){
+  window.close();
 }
