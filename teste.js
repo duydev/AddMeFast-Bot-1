184db6dd35c3e796ce428b0476461361655e4047
Xuxinha <filipe.bitt10@gmail.com>
2014-08-02 03:51:39 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,20 +1,2 @@
-var step = getCookie("amfstep").split(',')[0];
-var next = parseInt(step)+1;
-setCookie("amfstep",next+",0");
-document.getElementsByClassName("add_site_item_sub")[step].getElementsByTagName("a")[0].click();
-
-function setCookie(nome,str){
-  var expires = new Date();
-  expires.setTime(expires.getTime()+(30*24*60*60*1000));
-  expires = expires.toGMTString();
-  document.cookie = nome+"="+str+"; expires="+expires+"; path=/";
-}
-
-function getCookie(name){
-  var cookies = document.cookie.split("; ");
-  for(var i = 0; i < cookies.length; i++){
-    if(cookies[i].split("=")[0] == name)
-      return cookies[i].split("=")[1];
-  }
-  return "0,0";
-}
+window.open('','_parent','');
+  window.close();
